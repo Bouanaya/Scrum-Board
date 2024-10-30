dcd1e87002e3711369cc318f2abed736608e30cb
Bouanaya <bouanayasoufiane@gmail.com>
2024-10-30 14:14:16 +0100

    add activ  button Model

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -1,3 +1,26 @@
+// validate button
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('demo-form');
+    var submitButton = document.getElementById('submitBtn');
+
+    form.addEventListener('input', function() {
+        let allFilled = true;
+        var inputs = form.querySelectorAll('input');
+        inputs.forEach(function(input) {
+            if (input.value === '') {
+                allFilled = false;
+                console.log("hhhhhhhhhhhh");
+                return false;
+            }
+        });
+        submitButton.disabled = !allFilled;
+    });
+
+    // Trigger the input event to check the initial state
+    var event = new Event('input');
+    form.dispatchEvent(event);
+});
+
 const btnOpen = document.getElementById("show");
 var show = 0;
 
@@ -5,7 +28,7 @@ const openModel = () => {
     if (show == 0) {
         btnOpen.style.display ="block"
         show = 1
-        console.log("hhhhhh");
+        
     }
     else {
         btnOpen.style.display = "none"
